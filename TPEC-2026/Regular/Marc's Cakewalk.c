5842c7aa1fd6f3dee036c39be42ef4cd09af8b6f
0xdf <manthan56194@gmail.com>
2026-05-10 20:02:16 +0530

    Create Marc's Cakewalk.c

---
+++ TPEC-2026/Regular/Marc's Cakewalk.c
@@ -0,0 +1,84 @@
+#include<stdio.h>
+
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+void swap(int *a,int *b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(int *x,int start,int end)
+{
+    int pivot,pindex,i;
+
+    pivot = x[end];
+    pindex = start;
+
+    for(i = start; i < end; i++)
+    {
+        if(x[i] >= pivot)
+        {
+            swap(&x[i], &x[pindex]);
+            pindex = pindex + 1;
+        }
+    }
+
+    swap(&x[pindex], &x[end]);
+
+    return pindex;
+}
+
+void quicksort(int *x,int start,int end)
+{
+    if(start < end)
+    {
+        int i = partition(x, start, end);
+
+        // ====================== FROM HERE CODE TO BE WRITTEN ======================
+
+        quicksort(x, start, i - 1);
+        quicksort(x, i + 1, end);
+
+        // ======================= TILL HERE CODE TO BE WRITTEN ======================
+    }
+}
+
+int main()
+{
+    int n, calories_i, *calories;
+    int i;
+    long sum = 0;
+
+    scanf("%d", &n);
+
+    calories = malloc(sizeof(int) * n);
+
+    for(calories_i = 0; calories_i < n; calories_i++)
+    {
+        scanf("%d", &calories[calories_i]);
+    }
+
+    // ====================== FROM HERE CODE TO BE WRITTEN ======================
+
+    quicksort(calories, 0, n - 1);
+
+    for(i = 0; i < n; i++)
+    {
+        sum += (long)calories[i] * (1L << i);
+    }
+
+    printf("%ld\n", sum);
+
+    // ======================= TILL HERE CODE TO BE WRITTEN ======================
+
+    return 0;
+}
